a72cccf1c3d09b72d5d77c26ca6ec9a2fe93818a
Robin Chowdhury <robinch88@gmail.com>
2018-11-02 10:27:16 +0100

    Chapter 3 enum and streams

---
+++ file_line.ex
@@ -0,0 +1,37 @@
+defmodule File.Line do
+  def filtered_lines!(path) do
+    File.stream!(path)
+    |> Stream.map(&String.replace(&1, "\n", ""))
+  end
+
+  def lines_lengths!(path) do
+    path
+    |> filtered_lines!()
+    |> Enum.map(&String.length/1)
+  end
+
+  def longest_line_length!(path) do
+    path
+    |> filtered_lines!()
+    |> Stream.map(&String.length/1)
+    |> Enum.max()
+  end
+
+  def longest_line!(path) do
+    path
+    |> filtered_lines!()
+    |> Enum.max_by(&String.length/1)
+  end
+
+  def words_per_line!(path) do
+    path
+    |> filtered_lines!()
+    |> Enum.map(&word_count/1)
+  end
+
+  defp word_count(string) do
+    string
+    |> String.split()
+    |> Enum.count()
+  end
+end
