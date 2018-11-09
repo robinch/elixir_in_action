4961cee9ef20bb6258fd786a56cc4bd099bdef98
Robin Chowdhury <robinch88@gmail.com>
2018-11-09 17:19:09 +0100

    Created async Calculator

--- calculator/lib/calculator.ex
+++ calculator/lib/calculator.ex
@@ -1,18 +1,54 @@
 defmodule Calculator do
-  @moduledoc """
-  Documentation for Calculator.
-  """
+  def start() do
+    spawn(fn -> loop(0) end)
+  end
 
-  @doc """
-  Hello world.
+  def value(server_pid) do
+    send(server_pid, {:value, self()})
 
-  ## Examples
+    receive do
+      {:response, value} ->
+        value
+    end
+  end
 
-      iex> Calculator.hello()
-      :world
+  def add(server_pid, value), do: send(server_pid, {:add, value})
+  def sub(server_pid, value), do: send(server_pid, {:sub, value})
+  def mul(server_pid, value), do: send(server_pid, {:mul, value})
+  def div(server_pid, value), do: send(server_pid, {:div, value})
 
-  """
-  def hello do
-    :world
+  defp loop(current_value) do
+    new_value =
+      receive do
+        message -> process_message(current_value, message)
+      end
+
+    loop(new_value)
+  end
+
+  defp process_message(current_value, {:value, caller}) do
+    send(caller, {:response, current_value})
+    current_value
+  end
+
+  defp process_message(current_value, {:add, value}) do
+    current_value + value
+  end
+
+  defp process_message(current_value, {:sub, value}) do
+    current_value - value
+  end
+
+  defp process_message(current_value, {:mul, value}) do
+    current_value * value
+  end
+
+  defp process_message(current_value, {:div, value}) do
+    current_value / value
+  end
+
+  defp process_message(current_value, invalid_request) do
+    IO.puts("invalid request #{inspect(invalid_request)}")
+    current_value
   end
 end
