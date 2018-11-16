27e8bd5748a40aa7a4516a59eeb9ba3157906c44
Robin Chowdhury <robinch88@gmail.com>
2018-11-16 08:40:39 +0100

    Todo server

---
+++ todo_list/lib/todo_list/server.ex
@@ -0,0 +1,47 @@
+defmodule TodoList.Server do
+  def start() do
+    spawn(fn -> loop(TodoList.new()) end)
+    |> Process.register(:todo_server)
+  end
+
+  def add_entry(new_entry) do
+    send(:todo_server, {:add, new_entry})
+  end
+
+  def delete_entry(entry_id) do
+    send(:todo_server, {:delete, entry_id})
+  end
+
+  def entries(date) do
+    send(:todo_server, {:entries, self(), date})
+
+    receive do
+      {:todo_entries, entries} -> entries
+    after
+      5000 -> {:error, :timeout}
+    end
+  end
+
+  defp loop(todo_list) do
+    new_todo_list =
+      receive do
+        message ->
+          process_message(todo_list, message)
+      end
+
+    loop(new_todo_list)
+  end
+
+  defp process_message(todo_list, {:add, new_entry}) do
+    TodoList.add_entry(todo_list, new_entry)
+  end
+
+  defp process_message(todo_list, {:delete, entry_id}) do
+    TodoList.delete_entry(todo_list, entry_id)
+  end
+
+  defp process_message(todo_list, {:entries, caller, date}) do
+    send(caller, {:todo_entries, TodoList.entries(todo_list, date)})
+    todo_list
+  end
+end
---
+++ todo_list/test/todo_list/server_test.exs
@@ -0,0 +1,17 @@
+defmodule TodoList.ServerTest do
+  use ExUnit.Case
+
+  test "test everything" do
+    TodoList.Server.start()
+
+    TodoList.Server.add_entry(%{date: ~D[2018-11-15], title: "Dentist"})
+
+    entry = hd(TodoList.Server.entries(~D[2018-11-15]))
+    assert entry.title == "Dentist"
+    assert entry.date == ~D[2018-11-15]
+
+    TodoList.Server.delete_entry(entry.id)
+
+    assert TodoList.Server.entries(~D[2018-11-15]) == []
+  end
+end
