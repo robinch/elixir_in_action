fa638b95f2eb5ef8862da81d1354fc276e506674
Robin Chowdhury <robinch88@gmail.com>
2018-12-25 21:59:45 +0100

    Todo server uses GenServer

--- todo_list/lib/todo_list/server.ex
+++ todo_list/lib/todo_list/server.ex
@@ -1,34 +1,35 @@
 defmodule TodoList.Server do
+  use GenServer
+
   def start() do
-    ServerProcess.start(__MODULE__)
-    |> Process.register(:todo_server)
+    GenServer.start(__MODULE__, nil, name: __MODULE__)
   end
 
-  def init() do
-    TodoList.new()
+  def init(_) do
+    {:ok, TodoList.new()}
   end
 
   def add_entry(new_entry) do
-    ServerProcess.cast(:todo_server, {:add, new_entry})
+    GenServer.cast(__MODULE__, {:add, new_entry})
   end
 
   def delete_entry(entry_id) do
-    ServerProcess.cast(:todo_server, {:delete, entry_id})
+    GenServer.cast(__MODULE__, {:delete, entry_id})
   end
 
   def entries(date) do
-    ServerProcess.call(:todo_server, {:entries, date})
+    GenServer.call(__MODULE__, {:entries, date})
   end
 
   def handle_cast({:add, new_entry}, todo_list) do
-    TodoList.add_entry(todo_list, new_entry)
+    {:noreply, TodoList.add_entry(todo_list, new_entry)}
   end
 
   def handle_cast({:delete, entry_id}, todo_list) do
-    TodoList.delete_entry(todo_list, entry_id)
+    {:noreply, TodoList.delete_entry(todo_list, entry_id)}
   end
 
-  def handle_call({:entries, date}, todo_list) do
-    {TodoList.entries(todo_list, date), todo_list}
+  def handle_call({:entries, date}, _from, todo_list) do
+    {:reply, TodoList.entries(todo_list, date), todo_list}
   end
 end
