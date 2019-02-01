832dd12560a8e239950ea6c715d070a1aca108d5
Robin Chowdhury <robinch88@gmail.com>
2019-02-01 08:09:49 +0100

    Change Todo.Server to Agent

--- todo_cache/lib/todo/server.ex
+++ todo_cache/lib/todo/server.ex
@@ -1,44 +1,39 @@
 defmodule Todo.Server do
-  use GenServer, restart: :temporary
+  use Agent, restart: :temporary
 
   def start_link(name) do
-    GenServer.start_link(__MODULE__, name, name: via_tuple(name))
+    Agent.start_link(
+      fn ->
+        IO.puts("Starting to-do server for #{name}")
+        {name, Todo.Database.get(name) || Todo.List.new()}
+      end,
+      name: via_tuple(name)
+    )
+  end
+
+  def add_entry(pid, new_entry) do
+    Agent.cast(pid, fn {name, todo_list} ->
+      new_list = Todo.List.add_entry(todo_list, new_entry)
+      Todo.Database.store(name, new_list)
+      {name, new_list}
+    end)
+  end
+
+  def delete_entry(pid, entry_id) do
+    Agent.cast(pid, fn {name, todo_list} ->
+      new_list = Todo.List.delete_entry(todo_list, entry_id)
+      Todo.Database.store(name, new_list)
+      {name, new_list}
+    end)
+  end
+
+  def entries(pid, date) do
+    Agent.get(pid, fn {_name, todo_list} ->
+      Todo.List.entries(todo_list, date)
+    end)
   end
 
   defp via_tuple(name) do
     Todo.ProcessRegistry.via_tuple({__MODULE__, name})
-  end
-
-  def add_entry(pid, new_entry) do
-    GenServer.cast(pid, {:add, new_entry})
-  end
-
-  def delete_entry(pid, entry_id) do
-    GenServer.cast(pid, {:delete, entry_id})
-  end
-
-  def entries(pid, date) do
-    GenServer.call(pid, {:entries, date})
-  end
-
-  def init(name) do
-    IO.puts("Starting to-do server for #{name}")
-    {:ok, {name, Todo.Database.get(name) || Todo.List.new()}}
-  end
-
-  def handle_cast({:add, new_entry}, {name, todo_list}) do
-    new_list = Todo.List.add_entry(todo_list, new_entry)
-    Todo.Database.store(name, new_list)
-    {:noreply, {name, new_list}}
-  end
-
-  def handle_cast({:delete, entry_id}, {name, todo_list}) do
-    new_list = Todo.List.delete_entry(todo_list, entry_id)
-    Todo.Database.store(name, new_list)
-    {:noreply, {name, new_list}}
-  end
-
-  def handle_call({:entries, date}, _from, state = {_, todo_list}) do
-    {:reply, Todo.List.entries(todo_list, date), state}
   end
 end
--- todo_cache/test/todo_cache_test.exs
+++ todo_cache/test/todo_cache_test.exs
@@ -1,19 +1,44 @@
 defmodule Todo.CacheTest do
-  use ExUnit.Case
+  use ExUnit.Case, async: false
+
+  @dbs %{
+    bob: "DB_#{__MODULE__}_bob",
+    alice: "DB_#{__MODULE__}_alice"
+  }
+
+  setup_all do
+    Todo.System.start_link()
+
+    on_exit(fn ->
+      Enum.each(@dbs, fn {_key, db_name} ->
+        File.rm_rf!("persist/#{db_name}")
+      end)
+    end)
+
+    :ok
+  end
 
   test "server process" do
-    Todo.Cache.start_link()
-    bob_pid = Todo.Cache.server_process("bob")
+    bob_pid = Todo.Cache.server_process(@dbs.bob)
 
-    assert bob_pid != Todo.Cache.server_process("alice")
-    assert bob_pid == Todo.Cache.server_process("bob")
+    assert bob_pid != Todo.Cache.server_process(@dbs.alice)
+    assert bob_pid == Todo.Cache.server_process(@dbs.bob)
   end
 
   test "to-do operations" do
-    Todo.Cache.start_link()
-    alice = Todo.Cache.server_process("alice")
+    alice = Todo.Cache.server_process(@dbs.alice)
     Todo.Server.add_entry(alice, %{date: ~D[2018-12-19], title: "Dentist"})
+
     entries = Todo.Server.entries(alice, ~D[2018-12-19])
-    assert [%{date: ~D[2018-12-19], title: "Dentist"}] = entries
+
+    assert length(entries) == 1
+
+    entry = entries |> hd()
+
+    assert entry.date == ~D[2018-12-19]
+    assert entry.title == "Dentist"
+
+    Todo.Server.delete_entry(alice, entry.id)
+    assert Todo.Server.entries(alice, ~D[2018-12-19]) == []
   end
 end
