96c4a6ff161017b9278b117fc09698b92cb3ab92
Robin Chowdhury <robinch88@gmail.com>
2019-02-26 08:14:34 +0100

    Timeout unused TodoList

--- todo_cache/lib/todo/server.ex
+++ todo_cache/lib/todo/server.ex
@@ -1,36 +1,48 @@
 defmodule Todo.Server do
-  use Agent, restart: :temporary
+  use GenServer, restart: :temporary
+
+  @expiry_idle_timer :timer.seconds(10)
 
   def start_link(name) do
-    Agent.start_link(
-      fn ->
-        IO.puts("Starting to-do server for #{name}")
-        {name, Todo.Database.get(name) || Todo.List.new()}
-      end,
-      name: via_tuple(name)
-    )
+    GenServer.start_link(__MODULE__, name, name: via_tuple(name))
   end
 
   def add_entry(pid, new_entry) do
-    Agent.cast(pid, fn {name, todo_list} ->
-      new_list = Todo.List.add_entry(todo_list, new_entry)
-      Todo.Database.store(name, new_list)
-      {name, new_list}
-    end)
+    GenServer.cast(pid, {:add, new_entry})
   end
 
   def delete_entry(pid, entry_id) do
-    Agent.cast(pid, fn {name, todo_list} ->
-      new_list = Todo.List.delete_entry(todo_list, entry_id)
-      Todo.Database.store(name, new_list)
-      {name, new_list}
-    end)
+    GenServer.cast(pid, {:delete, entry_id})
   end
 
   def entries(pid, date) do
-    Agent.get(pid, fn {_name, todo_list} ->
-      Todo.List.entries(todo_list, date)
-    end)
+    GenServer.call(pid, {:entries, date})
+  end
+
+  def init(name) do
+    IO.puts("Starting to-do server for #{name}")
+    {:ok, {name, Todo.Database.get(name) || Todo.List.new()}, @expiry_idle_timer}
+  end
+
+  def handle_cast({:add, new_entry}, {name, todo_list}) do
+    new_list = Todo.List.add_entry(todo_list, new_entry)
+    Todo.Database.store(name, new_list)
+    {:noreply, {name, new_list}, @expiry_idle_timer}
+  end
+
+  def handle_cast({:delete, entry_id}, {name, todo_list}) do
+    new_list = Todo.List.delete_entry(todo_list, entry_id)
+    Todo.Database.store(name, new_list)
+    {:noreply, {name, new_list}, @expiry_idle_timer}
+  end
+
+  def handle_call({:entries, date}, _from, state = {_, todo_list}) do
+    {:reply, Todo.List.entries(todo_list, date), state, @expiry_idle_timer}
+  end
+
+  def handle_info(:timeout, {name, todo_list}) do
+    IO.puts("Stopping to-do server for #{name}")
+    {:stop, :normal, {name, todo_list}}
   end
 
   defp via_tuple(name) do
